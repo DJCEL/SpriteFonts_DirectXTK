fec5f257b1107146a9d5db1b2d21ed0d2dc48223
DJCEL <16272026+DJCEL@users.noreply.github.com>
2025-07-17 00:22:58 +0200

    Add files via upload

---
+++ MakeSpriteFont.bat
@@ -0,0 +1,13 @@
+MakeSpriteFont "Calibri" Calibri8.spritefont /FontSize:8
+MakeSpriteFont "Calibri" Calibri9.spritefont /FontSize:9
+MakeSpriteFont "Calibri" Calibri10.spritefont /FontSize:10
+MakeSpriteFont "Calibri" Calibri11.spritefont /FontSize:11
+MakeSpriteFont "Calibri" Calibri12.spritefont /FontSize:12
+MakeSpriteFont "Calibri" Calibri14.spritefont /FontSize:14
+MakeSpriteFont "Calibri" Calibri16.spritefont /FontSize:16
+MakeSpriteFont "Calibri" Calibri18.spritefont /FontSize:18
+MakeSpriteFont "Calibri" Calibri20.spritefont /FontSize:20
+MakeSpriteFont "Calibri" Calibri22.spritefont /FontSize:22
+MakeSpriteFont "Calibri" Calibri24.spritefont /FontSize:24
+MakeSpriteFont "Calibri" Calibri32.spritefont /FontSize:32
+pause
