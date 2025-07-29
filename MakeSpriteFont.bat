cb0ea5e7a14e9bbe8acd4cb700e2e00295db5fb0
djcel <16272026+DJCEL@users.noreply.github.com>
2025-07-29 05:40:37 +0200

    Update MakeSpriteFont.bat

--- MakeSpriteFont.bat
+++ MakeSpriteFont.bat
@@ -10,15 +10,15 @@ REM --- Font name ---
 set FONT = Calibri
 
 MakeSpriteFont "%FONT%" %FONT%8.spritefont /FontSize:8
-MakeSpriteFont "Calibri" Calibri9.spritefont /FontSize:9
-MakeSpriteFont "Calibri" Calibri10.spritefont /FontSize:10
-MakeSpriteFont "Calibri" Calibri11.spritefont /FontSize:11
-MakeSpriteFont "Calibri" Calibri12.spritefont /FontSize:12
-MakeSpriteFont "Calibri" Calibri14.spritefont /FontSize:14
-MakeSpriteFont "Calibri" Calibri16.spritefont /FontSize:16
-MakeSpriteFont "Calibri" Calibri18.spritefont /FontSize:18
-MakeSpriteFont "Calibri" Calibri20.spritefont /FontSize:20
-MakeSpriteFont "Calibri" Calibri22.spritefont /FontSize:22
-MakeSpriteFont "Calibri" Calibri24.spritefont /FontSize:24
-MakeSpriteFont "Calibri" Calibri32.spritefont /FontSize:32
+MakeSpriteFont "%FONT%" %FONT%9.spritefont /FontSize:9
+MakeSpriteFont "%FONT%" %FONT%10.spritefont /FontSize:10
+MakeSpriteFont "%FONT%" %FONT%11.spritefont /FontSize:11
+MakeSpriteFont "%FONT%" %FONT%12.spritefont /FontSize:12
+MakeSpriteFont "%FONT%" %FONT%14.spritefont /FontSize:14
+MakeSpriteFont "%FONT%" %FONT%16.spritefont /FontSize:16
+MakeSpriteFont "%FONT%" %FONT%18.spritefont /FontSize:18
+MakeSpriteFont "%FONT%" %FONT%20.spritefont /FontSize:20
+MakeSpriteFont "%FONT%" %FONT%22.spritefont /FontSize:22
+MakeSpriteFont "%FONT%" %FONT%24.spritefont /FontSize:24
+MakeSpriteFont "%FONT%" %FONT%32.spritefont /FontSize:32
 pause
