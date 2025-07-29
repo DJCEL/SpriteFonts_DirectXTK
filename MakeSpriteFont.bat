e8f22cf8c8773e0eb7e352a56a129c2aeb833e19
djcel <16272026+DJCEL@users.noreply.github.com>
2025-07-29 06:14:13 +0200

    Update MakeSpriteFont.bat

--- MakeSpriteFont.bat
+++ MakeSpriteFont.bat
@@ -7,11 +7,12 @@ echo =============================
 echo.
 
 REM --- Font name/size ---
-set FONT=Calibri
-set FONTSIZE_LIST=8,9,10,11,12,14,16,24,32
+set "FONT=Calibri"
+set "FONTSIZE_LIST=8 9 10 11 12 14 16 24 32"
 
 REM --- Run Command ---
 for %%i in (%FONTSIZE_LIST%) do (
-   MakeSpriteFont "%FONT%" %FONT%%%i.spritefont /FontSize:%%i
+   MakeSpriteFont "!FONT!" "!FONT!%%i.spritefont" /FontSize:%%i
 )
+
 pause
