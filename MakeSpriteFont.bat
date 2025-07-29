a8a0d2fd7b283deea4cb9e84e026e83bfb705112
djcel <16272026+DJCEL@users.noreply.github.com>
2025-07-29 05:55:55 +0200

    Update MakeSpriteFont.bat

--- MakeSpriteFont.bat
+++ MakeSpriteFont.bat
@@ -9,6 +9,7 @@ echo.
 REM --- Font name ---
 set FONT = Calibri
 set FONTSIZE = 8
+set NUMS = 8,9,10,11,12,13,14,16,24,32
 
 REM --- Run Command ---
 MakeSpriteFont "%FONT%" %FONT%%FONTSIZE%.spritefont /FontSize:%FONTSIZE%
